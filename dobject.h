c30aa118968f8b8ca66e2d0a7ca0e9fc2b6a6657
Wang Ding <ding.wang@harman.com>
2017-12-28 14:39:30 +0800

    The signal-slot mechanism implemented by indexing member funtion pointers.

--- dobject.h
+++ dobject.h
@@ -11,31 +11,53 @@ namespace ding
 {
 
 #define D_UNUSED(x) (void)x;
-#define EMIT(class_name,signal_name,...) emitSignal(#signal_name,&class_name::signal_name,##__VA_ARGS__);
+#define EMIT(class_name,signal_name,...) emitSignal(&class_name::signal_name,##__VA_ARGS__);
 
 class DObject;
 class DSignal
 {
 public:
-    virtual ~DSignal() = default;
+    using SigFunc = void (DObject::*)();
+    DSignal(const std::string& name, SigFunc func);
+    DSignal(const DSignal& other);
+    DSignal& operator=(const DSignal& other);
+    DSignal(DSignal&& other)noexcept;
+    DSignal& operator=(DSignal&& other)noexcept;
+    virtual ~DSignal();
+
     virtual void clear() = 0;
     virtual void removeSlotFromObj(std::weak_ptr<DObject> obj) = 0;
+
+    std::string getName()
+    {
+        return m_name;
+    }
+    SigFunc getFunc()
+    {
+        return m_func;
+    }
+private:
+    std::string m_name;
+    SigFunc m_func;
 };
 
 template<typename... Args>
 class DSignalImp : public DSignal
 {
-
 public:
-    using SlotFunc = void (DObject::*)(Args...);
+    using SigFuncImp = void (DObject::*)(Args...);
     struct DSlot
     {
         std::weak_ptr<DObject> m_obj;
-        SlotFunc m_slot;
-        DSlot(std::weak_ptr<DObject>&&obj, SlotFunc slot)
+        SigFuncImp m_slot;
+        DSlot(std::weak_ptr<DObject>&&obj, SigFuncImp slot)
             : m_obj(std::move(obj)), m_slot(slot)
         {}
     };
+
+    DSignalImp(const std::string& name, SigFuncImp func): DSignal(name, reinterpret_cast<DSignal::SigFunc>(func))
+    {
+    }
 
     virtual void clear()override
     {
@@ -52,21 +74,21 @@ public:
 
     void emit(Args&&... args)
     {
-        for(auto iter = m_slots.begin(); iter != m_slots.end(); ++iter)
+        for(auto it = m_slots.begin(); it != m_slots.end(); ++it)
         {
-            DSlot& slot = *iter;
+            DSlot& slot = *it;
             auto ptr = slot.m_obj.lock();
             auto func = slot.m_slot;
             if(ptr)
                 ptr->call(func, std::forward<Args>(args)...);
         }
     }
-    void addSlot(std::weak_ptr<DObject>&& obj, SlotFunc slot)
+    void addSlot(std::weak_ptr<DObject>&& obj, SigFuncImp slot)
     {
         m_slots.emplace_back(std::move(obj), slot);
     }
 
-    void removeSlot(std::weak_ptr<DObject>obj, SlotFunc slot)
+    void removeSlot(std::weak_ptr<DObject>obj, SigFuncImp slot)
     {
         m_slots.remove_if([&obj, &slot](const DSlot & item)
         {
@@ -96,49 +118,35 @@ public:
     bool addSignal(const std::string& name,
                    void(T::*signal)(Args...))
     {
-        D_UNUSED(signal)
-        if(m_signals.find(name) != m_signals.end())
+        auto it = findSignal(reinterpret_cast<DSignal::SigFunc>(signal));
+        if(it != m_signalList.cend())
             return false;
-        DSignal* metaSig = new DSignalImp<Args...>();
-        m_signals.emplace(name, metaSig);
+        m_signalList.emplace_back(
+            std::make_shared<DSignalImp<Args...>>
+            (name, static_cast<typename DSignalImp<Args...>::SigFuncImp>(signal)));
         return true;
     }
 
-    template<typename T,
-             typename... Args,
-             typename = std::enable_if_t<std::is_base_of<DObject, T>::value>>
-    bool connect(const std::string& signalname,
+    template < typename T,
+               typename U,
+               typename... Args,
+               typename = std::enable_if_t <
+                   std::is_base_of<DObject, T>::value
+                   && std::is_base_of<DObject, U>::value >>
+    bool connect(void (T::*signal)(Args...),
                  std::weak_ptr<DObject>&& obj,
-                 void (T::*slot)(Args...))
+                 void (U::*slot)(Args...))
     {
-        auto iter = m_signals.find(signalname);
-        if(iter == m_signals.end())
-        {
+        auto it = findSignal(reinterpret_cast<DSignal::SigFunc>(signal));
+        if(it == m_signalList.cend())
             return false;
-        }
-        auto sigImp = static_cast<DSignalImp<Args...>*>(iter->second);
-        sigImp->addSlot(std::move(obj), static_cast<typename DSignalImp<Args...>::SlotFunc>(slot));
+
+        auto sigImp = static_cast<DSignalImp<Args...>*>((*it).get());
+        sigImp->addSlot(std::move(obj), static_cast<typename DSignalImp<Args...>::SigFuncImp>(slot));
         return true;
     }
 
-    bool disconnect(const std::string& signalname, std::weak_ptr<DObject> obj)
-    {
-        if(signalname.empty())
-        {
-            m_signals.clear();
-            return true;
-        }
-        auto iter = m_signals.find(signalname);
-        if(iter == m_signals.end())
-        {
-            return false;
-        }
-        if(obj.lock())
-            iter->second->removeSlotFromObj(std::move(obj));
-        else
-            iter->second->clear();
-        return true;
-    }
+    bool disconnect(const std::string& signalname, std::weak_ptr<DObject> obj);
 
     template<typename T,
              typename... Args,
@@ -147,12 +155,12 @@ public:
                     std::weak_ptr<DObject> obj,
                     void (DObject::*slot)(Args...))
     {
-        auto iter = m_signals.find(signalname);
-        if(iter == m_signals.end())
+        auto it = findSignal(signalname);
+        if(it == m_signalList.cend())
         {
             return false;
         }
-        auto sigImp = static_cast<DSignalImp<Args...>*>(iter->second);
+        auto sigImp = static_cast<DSignalImp<Args...>*>((*it).get());
         sigImp->removeSlot(std::move(obj), slot);
         return true;
     }
@@ -161,19 +169,23 @@ public:
              typename... Args,
              typename... Args2,
              typename = std::enable_if_t<std::is_base_of<DObject, T>::value>>
-    void emitSignal(const std::string& name, void(T::*signal)(Args...), Args2 && ... args)
+    void emitSignal(void(T::*signal)(Args...), Args2 && ... args)
     {
-        D_UNUSED(signal)
-        auto iter = m_signals.find(name);
-        if(iter == m_signals.end())
+        auto it = findSignal(reinterpret_cast<DSignal::SigFunc>(signal));
+        if(it == m_signalList.cend())
         {
             return;
         }
-        auto sigImp = static_cast<DSignalImp<Args...>*>(iter->second);
+        auto sigImp = static_cast<DSignalImp<Args...>*>((*it).get());
         sigImp->emit(std::forward<Args>(args)...);
     }
 private:
-    std::unordered_map<std::string, DSignal*> m_signals;
+    std::vector<std::shared_ptr<DSignal>>::const_iterator
+                                       findSignal(DSignal::SigFunc signal)const;
+    std::vector<std::shared_ptr<DSignal>>::const_iterator
+                                       findSignal(std::string signal)const;
+
+    std::vector<std::shared_ptr<DSignal>> m_signalList;
 };
 
 }
