4f6ed980c0412db8761dfe5199cced6c607f8e02
Wang Ding <ding.wang@bst.ai>
2023-01-09 15:11:28 +0800

    feat: a message box simulator app.

--- app/main.cpp
+++ app/main.cpp
@@ -1,33 +1,42 @@
-#include <stdio.h>
+#include "simmsgbox.h"
+#include "ddispatcher.h"
+#include <thread>
 
-#pragma pack(16)
-struct align
-{
-	char a[18];
-	double b;
-	char c;
-	int d;
-	short e;
-};
-
-template <int N>
-int Fibonacci()
-{
-return Fibonacci<N-1>()+Fibonacci<N-2>();
-}
-
-template<> int Fibonacci<0>() { return 1;}
-template<> int Fibonacci<1>() { return 1;}
-
+using namespace ding;
+using namespace std::chrono_literals;
 int main()
 {
-	int a = -1;
-	unsigned b = 1;
-	if ( a > b)
-		printf("a>b, a = %d, b = %u\n",a,b);
-	else
-		printf("a<=b, a = %d, b = %u\n",a,b);
-	printf("sizeof align : %lu\n",sizeof(align));
-	printf("Fibonacci 3 = %d\n", Fibonacci<3>());
+	//create msg boxes
+	uint8_t id1 = 1, id2 = 2;
+	auto msgbox1 = makeObject<SimMsgBox>(id1);
+	auto msgbox2 = makeObject<SimMsgBox>(id2);
+	//create dispatcher
+	auto dispatcher = makeObject<DDispatcher>();
+	dispatcher->addRoutePath(new SimMsg(id1), &DDispatcher::output<0>);
+	dispatcher->addRoutePath(new SimMsg(id2), &DDispatcher::output<1>);
+	//connect
+	connect(msgbox1, &SimMsgBox::sendData, dispatcher, &DDispatcher::input);
+	connect(msgbox2, &SimMsgBox::sendData, dispatcher, &DDispatcher::input);
+	connect(dispatcher, &DDispatcher::output<0>, msgbox1, &SimMsgBox::received);
+	connect(dispatcher, &DDispatcher::output<1>, msgbox2, &SimMsgBox::received);
+
+	//start
+	dispatcher->start();
+
+	//put messages
+	Msg msg;
+	for (int i = 0; i < 10; ++i)
+	{
+		msg.src = (i%2)? id1 : id2;
+		msg.dst = (i%2)? id2 : id1;
+		msg.length = 3;
+		msg.data[0] = i;
+		msg.data[1] = i + 1;
+		msg.data[2] = i + 2;
+		msgbox1->send(msg);
+		msgbox2->send(msg);
+	}
+    std::this_thread::sleep_for(500ms);
+    dispatcher->stop();
 	return 0;
 }
---
+++ app/simmsgbox.h
@@ -0,0 +1,84 @@
+#ifndef SIM_MSG_BOX_H
+#define SIM_MSG_BOX_H
+
+#include "dobject.h"
+#include <iostream>
+
+using namespace ding;
+
+struct Msg
+{
+    uint8_t src;
+    uint8_t dst;
+    uint8_t length;
+    uint8_t data[10];
+};
+
+class SimMsg : public ding::DParam
+{
+public:
+    SimMsg(const Msg& msg): DParam("SimMsg"), m_content(msg){}
+    SimMsg(uint8_t dst): DParam("SimMsg"){ m_content.dst = dst;}
+    SimMsg(const SimMsg&) = default;
+    SimMsg(SimMsg&& other) noexcept: DParam(std::move(other))
+    {
+        m_content = other.m_content;
+    }
+    DParam *move() noexcept override
+    {
+        return new SimMsg(std::move(*this));
+    }
+
+    bool equals(const DParam& other)const override
+    {
+        if (DParam::equals(other))
+        {
+            auto rhs = static_cast<const SimMsg&>(other);
+            return m_content.dst == rhs.m_content.dst;
+        }
+        return false;
+    }
+
+    std::string toString()const
+    {
+        return string_format("src:%1, dst:%2, length:%3, data:%4 %5 %6", 
+            m_content.src, m_content.dst, m_content.length, m_content.data[0], m_content.data[1], m_content.data[2]);
+    }
+private:
+    Msg m_content;
+};
+
+class SimMsgBox : public ding::DObject
+{
+    public:
+    SimMsgBox(uint8_t id): DObject(__func__), m_id(id)
+    {
+        ADD_SIGNAL(SimMsgBox, sendData);
+    }
+    SIGNAL(SimMsgBox, sendData)
+    // SIGNAL(SimMsgBox, received)
+    void send(const Msg& msg)
+    {
+        if (msg.src != m_id)
+            return;
+        auto param = new SimMsg(msg);
+        sendData(*param);
+    }
+    void received(DParam& param)
+    {
+        auto msg = dynamic_cast<SimMsg*>(&param);
+        if (msg)
+        {
+            std::cout << "SimMsgBox " << m_id << " received message " << msg->toString() << std::endl;
+        }
+        else
+        {
+            std::cerr << "SimMsgBox " << m_id << " error message" << std::endl;
+        }
+    }
+
+    private:
+    uint8_t m_id;
+};
+
+#endif
